74a8dc35d57113518c650e71afeefb97239d2ed3
HORIE <26223147+suikan4github@users.noreply.github.com>
2026-06-21 13:09:44 +0900

    Fix the problem of script file.
    The env variable inside here-document was not correctly
    expanded. Fixed.
    
    Update README.md. Explain how to test and how to configure.

--- lifebook-lid-workaround.sh
+++ lifebook-lid-workaround.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-#SUSPEND_MODE=suspend
 # shellcheck disable=SC2034
-SUSPEND_MODE=suspend-then-hibernate
+SUSPEND_MODE=suspend
+#SUSPEND_MODE=suspend-then-hibernate
 #SUSPEND_MODE=hibernate
 
 # Script to suspend when the lid is closed.
-cat << 'EOF' | sudo tee /usr/local/bin/lid-monitor.sh
+cat << EOF | sudo tee /usr/local/bin/lid-monitor.sh
 #!/bin/bash
 
 # Prevent executing twice.
 PIDFILE=/var/run/lid-monitor.pid
-if [ -f "$PIDFILE" ] && kill -0 "$(cat "$PIDFILE")" 2>/dev/null; then
+if [ -f "\$PIDFILE" ] && kill -0 "\$(cat "\$PIDFILE")" 2>/dev/null; then
     exit 1
 fi
-echo $$ > "$PIDFILE"
+echo \$\$ > "\$PIDFILE"
 
 LID_STATE_FILE="/proc/acpi/button/lid/LID/state"
 
 while true; do
-    if [ -f "$LID_STATE_FILE" ]; then
+    if [ -f "\$LID_STATE_FILE" ]; then
         # Obtain lid state (open/closed)
-        STATE=$(awk '{print $2}' "$LID_STATE_FILE")
+        STATE=\$(awk '{print \$2}' "\$LID_STATE_FILE")
 
-        if [ "$STATE" = "closed" ]; then
+        if [ "\$STATE" = "closed" ]; then
             # Choose one of the three commands as a sleep action.
             systemctl $SUSPEND_MODE
             # Just after resume, sleep 5 seconds to prevent the false closed-state detection.
@@ -36,7 +36,7 @@ EOF
 sudo chmod +x /usr/local/bin/lid-monitor.sh
 
 # Deploy the script as Systemd service.
-cat << 'EOF' | sudo tee /etc/systemd/system/lid-monitor.service
+cat << EOF | sudo tee /etc/systemd/system/lid-monitor.service
 [Unit]
 Description=Monitor ACPI Lid State and Suspend
 After=multi-user.target
